483982230a69a5d215878af0242190a782915811
LytvyxEz <maxlytw@gmail.com>
2025-09-11 19:25:05 +0300

    Login/Registration validation/completing completed

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,9 +1,46 @@
+# models.py
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
 
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, username: str, email: str, phone_number: str, password: str, is_seller: bool = False):
+        try:
+            email = self.normalize_email(email)
+            user = self.model(
+                username=username,
+                email=email,
+                phone_number=phone_number,
+                is_seller=is_seller
+            )
+            
+            user.set_password(password)
+            user.save(using=self._db)
+        
+        except Exception as e:
+            raise e
+        
+        return user
+
+    def create_superuser(self, username: str, email: str, phone_number: str, password: str):
+        user = self.create_user(
+            username=username,
+            email=email,
+            phone_number=phone_number,
+            password=password,
+            is_seller=False
+        )
+        
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        
+        return user
 
 
 class MyUser(AbstractUser):
     is_seller = models.BooleanField(default=False)
+    phone_number = models.CharField(max_length=15, unique=True, null=True, blank=True)
+
+    objects = MyUserManager()
