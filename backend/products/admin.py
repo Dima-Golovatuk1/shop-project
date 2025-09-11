ab5afd91b87435708e894406e21db4a62c4aa5a9
Dmitro Oleynik <dmjtrooleinik@gmail.com>
2025-09-11 19:32:42 +0300

    refresh admin panel

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.html import format_html
 
-from products import Product, Category, SubCategory 
+from .models import Product, Category, SubCategory 
 
 # Register your models here.
 
