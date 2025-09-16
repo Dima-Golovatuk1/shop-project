892c37fa291a8a2817f11450f1a7fafebf7f3c52
LytvyxEz <maxlytw@gmail.com>
2025-09-16 19:08:37 +0300

    w

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,21 @@
+import { useState } from "react";
 import Header from "./header/header"
 import Catalog from "./catalog/catalog"
-import Registration from "./registration/registration";
-
 import Registration from "./registration/registration";
 import Login from "./login/login";
 
 
 function App() {
+    const [activLoginForm, setActivLoginForm] = useState(false);
+
     return (
         <>
             <Header />
-
-
-
             <Header setActivLoginForm={setActivLoginForm} />
 
             <main>
                 {activLoginForm && <Registration setActivLoginForm={setActivLoginForm} />}
+                {activLoginForm && <Login setActivLoginForm={setActivLoginForm} />}
 
                 <Catalog />
             {/* <Registration /> */}
