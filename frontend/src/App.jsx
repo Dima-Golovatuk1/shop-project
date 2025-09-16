7109eed007b075210246c40a151aed358d34c785
LytvyxEz <maxlytw@gmail.com>
2025-09-16 19:02:36 +0300

    w

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,34 @@
 import Header from "./header/header"
 import Catalog from "./catalog/catalog"
 import Registration from "./registration/registration";
-import Login from './login/login'
+
+import Registration from "./registration/registration";
+import Login from "./login/login";
 
 
 function App() {
     return (
         <>
             <Header />
+<<<<<<< Updated upstream
             <Catalog />
             {/* <Registration />
             <Login /> */}
+=======
+            <Header setActivLoginForm={setActivLoginForm} />
+
+            <main>
+                {activLoginForm && <Registration setActivLoginForm={setActivLoginForm} />}
+
+                <Catalog />
+            {/* <Registration /> */}
+            {/* <Login/> */}
+            </main>
+
+
+
+>>>>>>> Stashed changes
         </>
     );
 }
 
-
-export default App;
