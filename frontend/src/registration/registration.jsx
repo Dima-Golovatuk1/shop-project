3f649e3466e889ad6ac665ee66ef7027275b74bf
LytvyxEz <maxlytw@gmail.com>
2025-09-16 19:50:56 +0300

    ww

--- frontend/src/registration/registration.jsx
+++ frontend/src/registration/registration.jsx
@@ -15,35 +15,46 @@ function Registration({ setActivLoginForm }) {
     }
 
     return (
-        <section className="registration">
-            <div className="container">
-                <form ref={formRegistrRef} onSubmit={submitRegistr}>
-                    <label>
-                        Name
-                        <input name="name" type="text" placeholder="name" />
-                    </label>
-                    <label>
-                        Surname
-                        <input name="lastName" type="text" placeholder="surname" />
-                    </label>
-                    <label>
-                        Email
-                        <input name="email" type="text" placeholder="email" />
-                    </label>
-                    <label>
-                        Password
-                        <input name="password" type="password" placeholder="password" />
-                    </label>
-                    <label>
-                        Repeat password
-                        <input name="password2" type="password" placeholder="repeat password" />
-                    </label>
-                    <button type="submit">Register</button>
+        <div className="backdrop-registration" onClick={closeForm}>
+            <div className="registration" onClick={(e) => e.stopPropagation()}>
+                <button onClick={closeForm} className="registration__close-btn">
+                    <svg className="aside__btn-close__icon">
+                        <use href="/img/svg/symbol-defs.svg#icon-close"></use>
+                    </svg>
+                </button>
+                <section className="registration__content">
+                    <div className="container">
+                        <form ref={formRegistrRef} onSubmit={submitRegistr}>
+                            <label>
+                                Name
+                                <input name="name" type="text" placeholder="name" />
+                            </label>
+                            <label>
+                                Surname
+                                <input name="lastName" type="text" placeholder="surname" />
+                            </label>
+                            <label>
+                                Email
+                                <input name="email" type="text" placeholder="email" />
+                            </label>
+                            <label>
+                                Password
+                                <input name="password" type="password" placeholder="password" />
+                            </label>
+                            <label>
+                                Repeat password
+                                <input name="password2" type="password" placeholder="repeat password" />
+                            </label>
+                            <button type="submit">Register</button>
 
-                    <p><a href="/user/login">Already have an account?</a></p>
-                </form>
+                            <p>
+                                <a href="/user/login">Already have an account?</a>
+                            </p>
+                        </form>
+                    </div>
+                </section>
             </div>
-        </section>
+        </div>
     );
 }
 
