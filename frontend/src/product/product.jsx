4e661841fdd914752868abef9fbc2867eef96d3f
Dima-Golovatuk1 <146225071+Dima-Golovatuk1@users.noreply.github.com>
2025-09-19 17:54:57 +0300

    789

--- frontend/src/product/product.jsx
+++ frontend/src/product/product.jsx
@@ -11,20 +11,35 @@ function Product() {
     const [error, setError] = useState(null);
 
 
+    function submitAddCart(e) {
+        fetch(`http://localhost:8000/cart/api/add/${id}/`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log("Server Response:", data);
+            })
+            .catch(err => console.error("Error:", err));
+    }
+
+
     useEffect(() => {
         const API_URL = `http://localhost:8000/products/${id}/`;
-    
+
         async function fetchProduct() {
             try {
                 const response = await fetch(API_URL);
-    
+
                 if (!response.ok) {
                     throw new Error(`HTTP error! status: ${response.status}`);
                 }
-    
+
                 const data = await response.json();
                 console.log("Data:", data);
-    
+
                 setProduct(data.products[0]);
                 setError(null);
             } catch (e) {
@@ -34,10 +49,10 @@ function Product() {
                 setIsLoading(false);
             }
         }
-    
+
         fetchProduct();
     }, [id]);
-    
+
 
     if (isLoading) {
         return <div className="text-center mt-8 text-xl font-semibold">Downloading...</div>;
@@ -52,16 +67,20 @@ function Product() {
     return (
         <section className="product">
             <div className="container">
-                <h2 className="product__title">{product.title}</h2>
-                <img
-                    className="product__img"
-                    src={`http://localhost:8000${product.photo_url}`}
-                    alt={product.title}
-                />
-                <div className="product__div">
-                    <p className="product__text-description">{product.description}</p>
-                    <p className="product__text-price">{product.price}</p>
-                    <button className="product__btn">Купити</button>
+                <div className="product__card">
+                    <img
+                        className="product__img"
+                        src={`http://localhost:8000${product.photo_url}`}
+                        alt={product.title}
+                    />
+                    <div className="product__div">
+                        <h2 className="product__title">{product.title}</h2>
+                        <p className="product__text-description">{product.description}</p>
+                        <div className="product__div-buy">
+                            <p className="product__text-price">{product.price}</p>
+                            <button onClick={submitAddCart} className="product__btn">Купити</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
