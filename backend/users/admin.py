8124f6fc0ea16ec33ae73a0d3162d0da1aa64343
Dima-Golovatuk1 <146225071+Dima-Golovatuk1@users.noreply.github.com>
2025-09-19 19:13:00 +0300

    SUKA

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import MyUser # Remove MyUserManager
 
-# Register your models here.
+@admin.register(MyUser)
+class MyUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'phone_number', 'is_seller', 'is_staff', 'is_superuser')
