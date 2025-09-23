c2cb6b0dc5072474b99e236c00498509297a06be
LytvyxEz <maxlytw@gmail.com>
2025-09-23 18:54:38 +0300

    w

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -5,5 +5,5 @@ from .views import *
 urlpatterns = [
     path('api/', get_all_products, name='products'),
     path('<int:product_id>/', product, name='product'),
-    path('buy/<int:id_>/', buy_product, name='buy_product'),
+    path('buy/<int:product_id>/', buy_product, name='buy_product'),
 ]
